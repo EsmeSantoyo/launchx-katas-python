e1caaecf29b88307dbfceb237c28c5ef406d4a98
Esmeralda Santoyo <58286718+EsmeSantoyo@users.noreply.github.com>
2022-02-15 15:10:05 -0600

    add: solución kata 1

---
+++ launchX/CursoIntroPython-main/Módulo 1 - Primeros pasos/program.py
@@ -0,0 +1,18 @@
+#program.py
+sum = 1 + 2
+print(sum)
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+from datetime import date
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
+print(int(first_number) + int(second_number))
